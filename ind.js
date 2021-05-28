6619b1587ce6f3d2a03cfd03df3054876b38a9ca
Anamika Chourasia <70053428+Anamikachourasia@users.noreply.github.com>
2021-05-29 03:20:33 +0530

    Update ind.js

--- ind.js
+++ ind.js
@@ -1,4 +1,192 @@
-var list=document.getElementById('todo-list');
+//selectors
+const todoInput= document.querySelector(".todo-input")
+const todoButton= document.querySelector(".todo-button")
+const todoList= document.querySelector(".todo-list")
+const filterOption=document.querySelector(".filter-todo")
+//onclick task will bw added
+todoButton.addEventListener('click',addtodo);
+todoList.addEventListener('click',deleteCheck);
+//filterOption.addEventListener('click',filtertodo);
+//functions
+//adding
+todoInput.addEventListener('keyup',function(e)
+  {
+      if(e.keyCode===13)
+      {
+          addtodo();
+      }
+  })
+function addtodo(e)
+{
+    // prevent form from submitting
+    e.preventDefault();
+    //todo div
+    if(todoInput.value==="")
+    {
+        alert("Please add a task!");
+
+    }
+    else
+    {
+    const todoDiv=document.createElement("div")
+    todoDiv.classList.add('todo')
+    //create li
+    const newtodo= document.createElement("li")
+    newtodo.innerText= todoInput.value
+    newtodo.classList.add("todo-item")
+    todoDiv.appendChild(newtodo)
+    //create check button
+    // save to local storage
+    //saveLocaltodo(todoInput.value);
+    const completeButton = document.createElement("Button")
+    completeButton.innerHTML="<i class='fas fa-check'></i>";
+    completeButton.classList.add("complete-btn")
+    todoDiv.appendChild(completeButton)
+    //create delete button
+    const deleteButton = document.createElement("Button")
+    deleteButton.innerHTML="<i class='fas fa-trash'></i>";
+    deleteButton.classList.add("delete-btn")
+    todoDiv.appendChild(deleteButton)
+    //append to list
+    todoList.appendChild(todoDiv)
+    //clear input field
+
+    todoInput.value="";
+    }
+}
+//deleteing
+function deleteCheck(e)
+{
+    
+    const item=e.target;
+    //delete
+    if(item.classList[0]==='delete-btn')
+    {
+        const todo=item.parentElement;
+        //Animation
+        todo.classList.add("fall")
+        todo.addEventListener("transitionend",function(){
+            todo.remove;
+        })
+       
+    }
+    //check
+    if(item.classList[0]==='complete-btn')
+    {
+        const todo=item.parentElement;
+        todo.classList.toggle("completed");
+    }
+
+}
+// filte
+/*function filtertodo(e)
+{
+const todos = todoList.childNodes;
+todos.forEach(function(todo){
+    switch (e.target.value)
+    {
+        case "all":
+        todo.style.display="flex";
+        break;
+        case "Completed":
+         if(todo.classList.contains("completed"))
+         {
+             todo.style.display="flex";
+         } 
+         else{
+             todo.style.display="none";
+         }
+         break;
+         case "uncompleted":
+             if(todo.classList.contains("completed"))
+             {
+                 todo.style.display="none";
+             } 
+             else
+             {
+                 todo.style.display="flex";
+             } 
+             break;
+    }
+});
+
+}*/
+
+//adding to localstorage
+function saveLocaltodo(todo)
+{
+    //check if there is a thing there
+    let todos;
+    if(localStorage.getItem('todos')===null)
+    {
+        todos=[];
+    }
+    else
+    {
+        todos= JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos",JSON.stringyfy(todos))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var list=document.getElementById('todo-list');
 var inputBox=document.getElementById('todo-input');
 var btnAdd=document.getElementById('add-item');
 var btnUpdate=document.getElementById('update-item');
@@ -63,4 +251,5 @@ var btnRemove=document.getElementById('remove-item');
       list.removeChild(firstelement);
   })
 
+*/
 
